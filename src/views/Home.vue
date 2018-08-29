98f9cff0f0bc0abe8340a63c82acef473bbdb3f9
LynnAlan <191076550@qq.com>
2018-08-29 15:13:18 +0800

    change vue-class-components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <HelloWorld msg="hello, TypeScript App"/>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
+import Vue from "vue";
+import Component from "vue-class-component";
 import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
 
 @Component({
-  components: {
-  HelloWorld,
-  },
-  })
-export default class Home extends Vue {}
+    components: {
+        HelloWorld,
+    },
+})
+export default class Home extends Vue {
+
+}
 </script>
